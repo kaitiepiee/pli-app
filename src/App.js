6464caac629248892d2549b29b0a7200ebeed559
Kait Tighe <kaitlyntighe@gmail.com>
2024-02-12 14:14:15 +0800

    lots of changes v.3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import './pages/css/App.css';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import elementImage from './Element.png';
-import logo from './logo.png';
 
 import { Helmet } from 'react-helmet';
-
 
 const TITLE = 'Pascual Liner Inc.';
 
@@ -14,13 +12,11 @@ function App() {
     <div className="App">
       <Header />
       <Helmet>
-          <title>{ TITLE }</title>
-        </Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
       <div className="image">
         <img className="element" alt="Element" src={elementImage} />
-        <div className="image-text">Bringing Filipinos home.</div>
       </div>
-      
       <Footer />
     </div>
   );

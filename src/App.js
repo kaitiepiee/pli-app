a8e8e66a6f95d4406fe24f1b38e302a87c0f6e5f
Kait Tighe <kaitlyntighe@gmail.com>
2024-05-06 15:16:00 +0800

    updates home

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './pages/css/App.css';
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import elementImage from './Element.png';
+import elementImage2 from './Element2.png';
 
 import { Helmet } from 'react-helmet';
 
-const TITLE = 'Pascual Liner Inc.';
+const TITLE = 'Home | Pascual Liner Inc.';
 
 function App() {
   return (
@@ -14,8 +15,13 @@ function App() {
       <Helmet>
         <title>{TITLE}</title>
       </Helmet>
-      <div className="image">
+      <div className="image-container">
         <img className="element" alt="Element" src={elementImage} />
+      </div>
+      <div className="text-container">
+        <h2>Be a part of the PLI Family!</h2>
+        <div className="subtitle-home">Help Filipino communities reach their destination through our careers.</div>
+        <a className="button" href="/careers">Learn More</a>
       </div>
       <Footer />
     </div>
